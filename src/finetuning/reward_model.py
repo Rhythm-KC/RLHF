0a5ce469a2810a0e4a1f66e490f8ccff2d45f050
Rhythm-KC <rk7527@rit.edu>
2024-03-08 13:20:59 -0500

    removed input paramater comment

--- src/finetuning/reward_model.py
+++ src/finetuning/reward_model.py
@@ -1,22 +1,3 @@
-# Load model directly
-#from transformers import AutoTokenizer, AutoModelForCausalLM
-#import torch
-"""
-python ~/RLHF_Pipeline/src/finetuning/reward_model.py \
-    --model_name_or_path="gpt2"\
-    --output_dir="~/RLHF_Pipeline/output/reward_modeling_anthropic_hh" \
-    --per_device_train_batch_size=1 \
-    --num_train_epochs=1 \
-    --gradient_accumulation_steps=16 \
-    --gradient_checkpointing=True \
-    --learning_rate=1.41e-5 \
-    --report_to=none \
-    --remove_unused_columns=False \
-    --optim="adamw_torch" \
-    --logging_steps=10 \
-    --evaluation_strategy="steps" \
-    --max_length=1024
-"""
 import torch
 from datasets import load_dataset
 from tqdm import tqdm
